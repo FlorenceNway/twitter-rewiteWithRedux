0a03e86a21b0370f0519002193db4de9095bd126
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-05 23:55:26 +0100

    import router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Login from './components/Login';
+import Tweets from './components/Tweets';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
+
   return (
-    <div className="App">  
-        <Login />
+    <div className="App"> 
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route exact path="/tweets" component={Tweets}/>
+        </Switch>
+      </Router> 
+        
     </div>
   );
 }

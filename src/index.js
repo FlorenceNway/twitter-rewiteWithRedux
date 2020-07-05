b829b4a11e88ddf1e0b732c6f5a968871a4546e3
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-05 02:25:56 +0100

    store created with tweetDetails reducer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import tweetDetails from './store/login.reducer';
 import App from './App';
 
+const store = createStore(
+  tweetDetails
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-

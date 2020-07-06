52fe93131e2517997c9679613027f17b9872cc4b
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-07 00:45:45 +0100

    render userProfile part

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
+import { useHistory } from "react-router-dom";
 import '../style/Tweet.scss'
 
 const Tweets = () => {
     const dispatch = useDispatch()
+    const history = useHistory();
     const tweetDetails = useSelector(state => state.tweetDetails)
+
+    useEffect(() => {
+        if (!tweetDetails.length) {
+          history.push("/");
+        } 
+      }, []);
   
-    const {name} = tweetDetails 
-    return <div>
-                <div class="avatar">
-                    <input type='file' class='choose_file'/> 
+    return tweetDetails.length !== 0 ? (
+            <div className='userInfo'>        
+                <div className="avatar">
+                    <input type='file' className='choose_file' onChange={chooseFileHandler}/> 
                 </div>
-                <div class="userDetail">
-                    <p><b>{name}</b></p>
-                    <p>@{name}</p>
-                    <p>London,UK</p>
-                    <p><b>245</b> Followers</p>
-                    <p><b>132</b> Followings</p>
-                </div>
-        
-            </div> 
+                    {
+                        tweetDetails.map(tweet => (
+                            <div className="userDetail">
+                                <p><b>{tweet.name}</b></p>
+                                <p>@{tweet.name}</p>
+                                <p>London,UK</p>
+                                <p><b>245</b> Followers</p>
+                                <p><b>132</b> Followings</p>
+                            </div>
+                        ))
+                    }
+            </div> ): ("") 
 
 }
 export default Tweets;

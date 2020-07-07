b58c02150c8204ebc03269b5049dd90b4f6dbcaf
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-07 15:45:24 +0100

    render tweets on page

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -1,20 +1,37 @@
-import React, {useEffect} from 'react';
+import React, {useState,useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import { useHistory } from "react-router-dom";
+import API from './API';
 import '../style/Tweet.scss'
 
 const Tweets = () => {
     const dispatch = useDispatch()
     const history = useHistory();
+    const [allUsers, setAllUsers] = useState([])
+    const [allTweets, setAllTweets] = useState([])
     const tweetDetails = useSelector(state => state.tweetDetails)
 
     useEffect(() => {
         if (!tweetDetails.length) {
           history.push("/");
         } 
-      }, []);
+        else {
+            API.getAllUsers().then((users) => {
+                setAllUsers(users);
+            });
+            API.getTweets().then((tweets) => {
+                setAllTweets(tweets);
+            });
+        }  
+    }, []);
+    
+    
+
+    const chooseFileHandler = () => {
+
+    }
   
-    return tweetDetails.length !== 0 ? (
+    return tweetDetails.length !== 0 ? <div className='content'>
             <div className='userInfo'>        
                 <div className="avatar">
                     <input type='file' className='choose_file' onChange={chooseFileHandler}/> 
@@ -30,7 +47,36 @@ const Tweets = () => {
                             </div>
                         ))
                     }
-            </div> ): ("") 
+            </div> 
+            <div className='tweets'>
+                {allTweets.map(tweet => {
+                   
+                    const {userId, date, name, likes, retweets, comments, content} = tweet
+                    const whoTweet = allUsers.filter(user => user.id === userId)
+                    const whoTweetName = whoTweet[0].name
+                    
+                    return  <div className='tweet'>
+                                <div className='user_data'>
+                                    <p>{whoTweetName}</p>
+                                    <p>{date}</p>
+                                </div>
+                                <div className='tweetContent' >
+                                    <p>{content}</p>
+                                </div>
+                                <div className='like_share'>
+                                    <p><img src={require('../images/heart.svg')} alt='likes' id={tweet.id}/><span className='like_Btn'>{likes}</span></p>
+                                    <p><img src={require('../images/retweet.svg')} alt='retweets' id={tweet.id}/><span className='retweet_Btn'>{retweets}</span></p>
+                                    <p id={whoTweet[0].id}><img src={require('../images/comment.svg')} alt='comments'id={tweet.id}/><span className='comment_Btn'>{comments.length}</span></p> 
+                                </div>
+                            </div>
+            
+                  
+
+                })}
+               
+            </div>
+            
+        </div> : ("") 
 
 }
 export default Tweets;

a230828921ee4d467660c25e64333a919266ee75
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-07 22:54:25 +0100

    like button click reducer

--- src/store/login.reducer.js
+++ src/store/login.reducer.js
@@ -1,15 +1,23 @@
 
 let initialState = {
-    tweetDetails : []
+    userDetails: [],
+    tweetDetails : [],
+    commentDetails: []
 }
 
 const tweetDetails = (state = initialState, {type,payload}) => {
 
     switch(type) {
-        case 'LOGIN_CHECK':
+        case 'SAVE_LOGIN_USER':
             console.log('payload', payload)
             return {...state, tweetDetails: [...state.tweetDetails, ...payload]};
-
+        case 'LIKES_CLICK':
+            const localTweetDetails = [...state.tweetDetails[0].tweets]    
+            const selectedTweet = localTweetDetails.filter(localTweet => localTweet.id === payload)
+            selectedTweet[0].likes = parseInt(selectedTweet[0].likes) + 1
+            console.log(localTweetDetails, [...state.tweetDetails[0].tweets])
+            return {...state, tweetDetails: [...state.tweetDetails, ...localTweetDetails]};
+            
         default:
             return state;
     }

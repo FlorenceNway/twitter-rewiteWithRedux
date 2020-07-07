a99a90c429013940b8692228026aca00f6eaebd6
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-07 22:53:40 +0100

    likes action

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -1,6 +1,7 @@
 import React, {useState,useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import { useHistory } from "react-router-dom";
+import {likesClick} from '../store/login.actions';
 import API from './API';
 import '../style/Tweet.scss'
 
@@ -25,15 +26,12 @@ const Tweets = () => {
         }  
     }, []);
     
-    
 
     const chooseFileHandler = (e) => {
         const file_input = e.target.files[0];
        
         if(file_input) {
                 const avatar_div = document.querySelector('.avatar')
-                //const file = file_input.files[0]
-                console.log('file', file_input)
                 const reader  = new FileReader();
 
                 reader.onload = (e) => {
@@ -41,26 +39,29 @@ const Tweets = () => {
                     avatar_div.style.backgroundSize = 'cover'   
                 }
                 reader.readAsDataURL(file_input);     
-         }
+        }
+    }
+
+    const likesHandler = (id) => {
+        dispatch(likesClick(id))
+
+        
     }
   
     return tweetDetails.length !== 0 ? <div className='content'>
             <div className='userInfo'>        
                 <div className="avatar">
                     <input type='file' className='choose_file' onChange={chooseFileHandler}/> 
+                </div>    
+                <div className="userDetail">
+                    <p><b>{tweetDetails[0].name}</b></p>
+                    <p>@{tweetDetails[0].name}</p>
+                    <p>London,UK</p>
+                    <p><b>245</b> Followers</p>
+                    <p><b>132</b> Followings</p>
                 </div>
-                    {
-                        tweetDetails.map(tweet => (
-                            <div className="userDetail">
-                                <p><b>{tweet.name}</b></p>
-                                <p>@{tweet.name}</p>
-                                <p>London,UK</p>
-                                <p><b>245</b> Followers</p>
-                                <p><b>132</b> Followings</p>
-                            </div>
-                        ))
-                    }
             </div> 
+
             <div className='tweets'>
                 {allTweets.map(tweet => {
                    
@@ -77,7 +78,7 @@ const Tweets = () => {
                                     <p>{content}</p>
                                 </div>
                                 <div className='like_share'>
-                                    <p><img src={require('../images/heart.svg')} alt='likes' id={tweet.id}/><span className='like_Btn'>{likes}</span></p>
+                                    <p><img src={require('../images/heart.svg')} alt='likes' onClick={() => likesHandler(tweet.id)}/><span className='like_Btn'>{likes}</span></p>
                                     <p><img src={require('../images/retweet.svg')} alt='retweets' id={tweet.id}/><span className='retweet_Btn'>{retweets}</span></p>
                                     <p id={whoTweet[0].id}><img src={require('../images/comment.svg')} alt='comments'id={tweet.id}/><span className='comment_Btn'>{comments.length}</span></p> 
                                 </div>
@@ -87,6 +88,6 @@ const Tweets = () => {
             </div>
             
         </div> : ("") 
-
 }
+
 export default Tweets;

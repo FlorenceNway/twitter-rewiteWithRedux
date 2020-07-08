7760fd347dfa42cc63c246cf5be775713619afc9
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-09 00:06:17 +0100

    reactions click reducer added

--- src/store/login.reducer.js
+++ src/store/login.reducer.js
@@ -11,11 +11,11 @@ const tweetDetails = (state = initialState, {type,payload}) => {
         case 'SAVE_LOGIN_USER':
             console.log('payload', payload)
             return {...state, userDetails: [...state.userDetails, ...payload]};
-        case 'LIKES_CLICK':
-            const {id, tweets} = payload
+        case 'REACT_CLICK':
+            const {id, react, tweets} = payload
             const selectedTweet = tweets.filter(tweet => tweet.id === id)
-            selectedTweet[0].likes = parseInt(selectedTweet[0].likes) + 1
-            return {...state, tweetDetails: [...state.tweetDetails, ...selectedTweet]};
+            selectedTweet[0][react] = parseInt(selectedTweet[0][react]) + 1
+            return {...state, tweetDetails: [...tweets]};
 
             // const localUserDetails = [...state.userDetails[0].tweets]    
             // const selectedTweet = localUserDetails.filter(localTweet => localTweet.id === payload)

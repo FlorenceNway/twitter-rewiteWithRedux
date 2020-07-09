e38edd2cb56f45e0876daba12d7dfef2d21973fc
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-09 23:47:46 +0100

    removed comments

--- src/store/login.reducer.js
+++ src/store/login.reducer.js
@@ -16,12 +16,6 @@ const tweetDetails = (state = initialState, {type,payload}) => {
             const selectedTweet = tweets.filter(tweet => tweet.id === id)
             selectedTweet[0][react] = parseInt(selectedTweet[0][react]) + 1
             return {...state, tweetDetails: [...tweets]};
-
-            // const localUserDetails = [...state.userDetails[0].tweets]    
-            // const selectedTweet = localUserDetails.filter(localTweet => localTweet.id === payload)
-            // selectedTweet[0].likes = parseInt(selectedTweet[0].likes) + 1
-            // console.log(localUserDetails, [...state.userDetails[0].tweets])
-            // return {...state, userDetails: [...state.userDetails, ...localUserDetails]};
             
         default:
             return state;

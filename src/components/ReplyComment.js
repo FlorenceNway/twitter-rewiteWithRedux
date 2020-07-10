01eaebea31bdfb637d7c9e8dc82759e47e4e7c41
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-10 22:13:27 +0100

    added class for styling

--- src/components/ReplyComment.js
+++ src/components/ReplyComment.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
+import '../style/ReplyComment.scss';
 
-const ReplyComment = () => {
-    return (<>
+const ReplyComment = ({id}) => {
+    return (<div className="commentBox" id={id}>
             <textarea className='commentTextarea' placeholder='Your Comment'></textarea>
             <div className='tweetBtnArea'>
                 <img className='bakArow' src={require('../images/backarrow.svg')} alt="backArrow"/>
                 <button className='replyBtn'>Reply</button>
             </div>
-         </>       
+         </div>       
     )
 }
 

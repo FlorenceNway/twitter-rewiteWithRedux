af26baf89ec991005fee2be72bebe77683f99afd
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-11 23:59:05 +0100

    tweet detail component route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Login from './components/Login';
 import Tweets from './components/Tweets';
+import Tweet from './components/TweetDetail';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Login}/>
           <Route exact path="/tweets" component={Tweets}/>
+          <Route exact path="/tweet/:id" component={Tweet}/>
         </Switch>
       </Router> 
         

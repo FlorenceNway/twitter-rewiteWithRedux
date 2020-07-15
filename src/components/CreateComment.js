d28873c1910933e3b42196790639511a4382377d
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-15 23:47:00 +0100

    init createComment

---
+++ src/components/CreateComment.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ const CreateComment = () => {
+    return (
+        <img className="messageBtn" src={require('../images/createNewMessage.png')}/>
+    )
+}
+
+export default CreateComment;

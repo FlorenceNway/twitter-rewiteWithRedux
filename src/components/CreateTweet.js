8b2fbfd56eb9ac651bcd314d43dfd974ef319e76
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-15 23:31:04 +0100

    tweet btn handler

--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import '../style/CreateTweet.scss'
+import React,{useState} from 'react';
+import '../style/CreateTweet.scss';
+import API from './API';
 import { useHistory } from "react-router-dom";
+import {useSelector} from 'react-redux';
 
  const CreateTweet = () => {
     const history = useHistory();
+    const userDetails = useSelector(state => state.userDetails)
+    const [textInput, setTextInput] = useState("")
 
     const backArrowClickHandler = () => {
         history.push("/Tweets");
+    }
+
+    const textInpuHandler = (e) => {
+        setTextInput(e.target.value)
+    }
+
+    const tweetBtnHandler = () => {
+        const id = userDetails[0].id
+        API.postTweet(id, textInput)
     }
 
     return (
@@ -14,10 +27,10 @@ import { useHistory } from "react-router-dom";
             <div className="tweetAsUwish">
                     <div className='navigateArea'>
                         <img src={require('../images/tweetBackArrow.svg')} alt="backArrow" className='backtotweets' onClick={backArrowClickHandler}/>
-                        <button className='createNewTweetBtn'>Tweet</button>
+                        <button className='createNewTweetBtn' onClick={tweetBtnHandler}>Tweet</button>
                     </div>
                     <div className='tweetText'>
-                        <textarea name="tweet" cols="30" rows="9" placeholder="What's on your mind"></textarea>
+                        <textarea name="tweet" cols="30" rows="9" placeholder="What's on your mind" onChange={textInpuHandler}></textarea>
                     </div>
             </div>
         </div>

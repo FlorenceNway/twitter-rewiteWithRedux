2f1542500a1fd33be845619a957de8bfb9519d1c
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-17 23:55:05 +0100

    reply component

--- src/components/ReplyComment.js
+++ src/components/ReplyComment.js
@@ -2,14 +2,14 @@ import React from 'react';
 import '../style/ReplyComment.scss';
 
 const ReplyComment = ({id}) => {
-    return (<div className="commentBox" id={id}>
-            <textarea className='commentTextarea' placeholder='Your Comment'></textarea>
-            <div className='tweetBtnArea'>
-                <img className='bakArow' src={require('../images/backarrow.svg')} alt="backArrow"/>
-                <button className='replyBtn'>Reply</button>
-            </div>
-         </div>       
-    )
+    return <div className="commentBox" id={id}>
+                <textarea className='commentTextarea' placeholder='Your Comment'></textarea>
+                <div className='tweetBtnArea'>
+                    <img className='bakArow' src={require('../images/backarrow.svg')} alt="backArrow"/>
+                    <button className='replyBtn'>Reply</button>
+                </div>
+            </div>       
+    
 }
 
 export default ReplyComment;

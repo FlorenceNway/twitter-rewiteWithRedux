93d5f849dbf38a56a45e25bee4f0aed9e28b2ee8
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-20 23:32:30 +0100

    comment click action

--- src/store/twitter.actions.js
+++ src/store/twitter.actions.js
@@ -8,3 +8,8 @@ export const reactClick = (id,react, allTweets) => ({
     type: 'REACT_CLICK',
     payload: {id:id, react:react, tweets:allTweets}
 })
+
+export const commentClick = (id, comments) => ({
+    type: 'COMMENT_CLICK',
+    payload: {id:id, comments: comments}
+})

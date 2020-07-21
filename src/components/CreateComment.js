03705c7a6c0fee9d56daada4ea59bd0966dd63bc
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-21 23:54:53 +0100

    remove wrapper

--- src/components/CreateComment.js
+++ src/components/CreateComment.js
@@ -1,12 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 
  const CreateComment = ({messageIconHandler}) => {
 
-    return (<>
-        <img className="messageBtn" src={require('../images/createNewMessage.png')} onClick={messageIconHandler}/>
+    return <img className="messageBtn" src={require('../images/createNewMessage.png')} onClick={messageIconHandler}/>
         
-    </>)
 }
 
 export default CreateComment;

d19249157c8ef666ff7faa8c1e51009ad2978f83
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-07-22 23:42:12 +0100

    reply handlers works in own component

--- src/components/ReplyComment.js
+++ src/components/ReplyComment.js
@@ -1,24 +1,27 @@
 import React, {useState} from 'react';
 import API from './API';
-
+import {commentClick} from '../store/twitter.actions';
 import {useDispatch} from 'react-redux';
 import '../style/ReplyComment.scss';
 
-const ReplyComment = ({commentHandler, replyHandler, replybackArrowHandler, comment}) => {
+//const ReplyComment = ({commentHandler, replyHandler, replybackArrowHandler, comment}) => {
+const ReplyComment = ({userId, tweetId, comments, setComments, replybackArrowHandler}) => {
 
-    //const dispatch = useDispatch();
-    // const [comment , setComment] = useState("");
+    const dispatch = useDispatch();
+    const [comment , setComment] = useState("");
 
-    // const commentHandler = (e) => {
-    //     setComment(e.target.value)
-    // }
+    const commentHandler = (e) => {
+        setComment(e.target.value)
+    }
 
-    // const replyHandler = () =>{
-    //     const newComment = comment
-    //     API.postComment(userId, tweetId, newComment)
-    //     setComment("")
-    //     dispatch(commentClick(userId, tweetId, comments, newComment))
-    // }
+    const replyHandler = () =>{
+        const newComment = comment
+        API.postComment(userId, tweetId, newComment)
+        setComment("")
+        dispatch(commentClick(userId, tweetId, comments, newComment))
+        console.log(comments)
+        setComments(comments)
+    }
 
     return <div className="commentBox">
                 <textarea className='commentTextarea' placeholder='Your Comment' value={comment} onChange={commentHandler}></textarea>

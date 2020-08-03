f1c41c19c842a9fd9d193623abbc3fcaec8b17ba
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-08-03 02:01:11 +0100

    test logs

--- src/store/twitter.reducer.js
+++ src/store/twitter.reducer.js
@@ -11,19 +11,23 @@ const tweetDetails = (state = initialState, {type,payload}) => {
     switch(type) {
         case 'SAVE_LOGIN_USER':
             return {...state, userDetails: [...state.userDetails, ...payload]};
+            
         case 'REACT_CLICK':
             console.log('payload', payload)
             const {id, react, tweets} = payload
             const selectedTweet = tweets.filter(tweet => tweet.id === id)
             selectedTweet[0][react] = parseInt(selectedTweet[0][react]) + 1
             return {...state, tweetDetails: [...tweets]};
+
         case 'COMMENT_CLICK':
-            console.log('comment payload', payload) 
+            
             const d = new Date()
             const yy = d.getFullYear()
             const mm = d.getMonth() + 1
             const dd = d.getDate()
             const {userId, tweetId, comments, newComment} = payload;
+            console.log("comment payload", payload); 
+
             const allComments = comments
             allComments.push({
                 userId: userId,
